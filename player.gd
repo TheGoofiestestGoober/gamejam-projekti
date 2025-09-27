f5afc3356e508578b231b4b1ac994a8945266690
TheGoofiestestGoober <czeizler.gabriel@gmail.com>
2025-09-27 16:12:55 +0300

    added player death

--- player.gd
+++ player.gd
@@ -4,11 +4,19 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func die():
+	get_tree().reload_current_scene()
+	
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if position.y >= 525:
+			die()
+			
+			
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
