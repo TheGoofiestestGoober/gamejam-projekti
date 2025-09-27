f2bb71b5abf910587c55d92cfaa95f405d02ddbb
TheGoofiestestGoober <czeizler.gabriel@gmail.com>
2025-09-27 20:42:50 +0300

    .

--- enemy.gd
+++ enemy.gd
@@ -1,24 +1,18 @@
 extends CharacterBody2D
 
-const SPEED = 60.0
-var direction = -1  # -1 for left, 1 for right
+const speed = 100
 
-func _physics_process(delta):
-	velocity.x = SPEED * direction
+var direction = 1
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	velocity.x = direction * speed
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+
+
 	move_and_slide()
-
-
-	if is_on_wall() or is_at_edge():
-		direction *= -1
-
-func is_at_edge() -> bool:
-	var raycast_pos = global_position + Vector2(direction * 10, 16)
-	var space_state = get_world_2d().direct_space_state
-
-	var ray_params = PhysicsRayQueryParameters2D.new()
-	ray_params.from = raycast_pos
-	ray_params.to = raycast_pos + Vector2(0, 10)
-	ray_params.exclude = [self]
-
-	var result = space_state.intersect_ray(ray_params)
-	return result.empty()
