9955fa51f812781d7a4ac30c48af395dd99ac1c5
TheGoofiestestGoober <czeizler.gabriel@gmail.com>
2025-09-27 17:13:53 +0300

    Added sword

--- player.gd
+++ player.gd
@@ -4,18 +4,45 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var Sword_On_Cooldown = false
+
+
+
 func die():
 	get_tree().reload_current_scene()
 	
+func attack():
+	
+	if not Sword_On_Cooldown:
+		$Sword.visible = true
+		$Sword/CollisionShape2D.disabled = false
 
+		await get_tree().create_timer(0.5).timeout
+
+		$Sword.visible = false
+		$Sword/CollisionShape2D.disabled = true
+		Sword_On_Cooldown = true
+		async_hide_sword()
+func async_hide_sword() -> void:
+	await get_tree().create_timer(0.3).timeout
+	$Sword.visible = false
+	$Sword/CollisionShape2D.disabled = true
+
+	await get_tree().create_timer(0.3).timeout
+	Sword_On_Cooldown = false
+	
+		
+	
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		if position.y >= 525:
 			die()
-			
+	if Input.is_action_just_pressed("attack"):
+		attack()	
 			
 
 	# Handle jump.
